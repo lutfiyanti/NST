c6c5392885c24139389b82b1a528b8707ef1cea0
Lutfy <37364743+lutfiyanti@users.noreply.github.com>
2018-10-18 22:01:38 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import requests, json
+
+
+import errno
+import os
+import sys, random
+import tempfile
+import requests
+import re
+import requests, json
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent,
+)
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('fZklMpUf7UueAqA1DtHj3V8+Udz9toJ1AEf/7iqchW3od8eoDfdi0/0UV5t/PqSfnHtVQZLtie3eOu6JgKKfpbUr40rMWj1RfmdyxNSIOGJm6Ppd7wZGGl0zfWKONWEBlifqBPiZcjoDWWai+MdnwAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('8d34f33a911c087bfa231108a614b282')
+#===========[ NOTE SAVER ]=======================
+notes = {}
+
+#input mencari
+def cariproduk(nama_produk):
+    URLproduk = "http://api.agusadiyanto.net/halal/?menu=nama_produk&query=" + nama_produk
+    r = requests.get(URLproduk)
+    produk = r.json()
+    err = "data tidak ditemukan"
+    
+    status = produk['status']
+    if(status == "success"):
+        nama_produk = produk['data'][0]['nama_produk']
+        nomor_sertifikat = produk['data'][0]['nomor_sertifikat']
+        nama_produsen = produk['data'][0]['nama_produsen']
+        berlaku_hingga = produk['data'][0]['berlaku_hingga']
+        tes = produk['data'][0]['tes']
+        
+
+        # munculin semua, ga rapi, ada 'u' nya
+        # all_data = data['teman'][0]
+        data= "Nama Produk : "+nama_produk+"\nNomor Sertifikat : "+nomor_sertifikat+"\nNama Produsen : "+nama_produsen+"\nBerlaku Hingga : "+berlaku_hingga+"\nTes : "+tes
+        return print (data)
+        # return all_data
+
+    elif(status == "error"):
+        return print (err)
+
+
+cariproduk ("marjan")
+
+
+
+# Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text #simplify for receive message
+    sender = event.source.user_id #get usesenderr_id
+    gid = event.source.sender_id #get group_id
+    profile = line_bot_api.get_profile(sender)
+
+    data=text.split('-')
+    if(data[0]=='find'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cariproduk(data[1])))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Keyword salah, untuk mencari produk tuliskan find-(nama produk) "))
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
